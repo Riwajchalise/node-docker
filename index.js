dc03764b93b4b342da90192ba2e873e51c9d2d25
asterdio-riwaj <riwaj.chalise@asterdio.com>
2022-02-06 22:40:42 +0545

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express()
+
+app.get("/", (req,res) => {
+    res.send("<h2>This is vettyo API. Please refere this docs</h2>");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listning on port ${port}`));
