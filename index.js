d14fcfe9938bb12d39e8c08d81eebbf466b6fc51
asterdio-riwaj <riwaj.chalise@asterdio.com>
2022-03-09 13:16:22 +0545

    Run the application in developement mode

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express()
 
 app.get("/", (req,res) => {
-    res.send("<h2>This is vettyo API. Please refere this docs</h2>");
+    res.send("<h2>This is vettyo API. Please refere this docs!!</h2>");
 });
 
 const port = process.env.PORT || 3000;
